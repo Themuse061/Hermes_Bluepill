3a29e5dfadf8bd65b7c10481844081bee109ceed
Themuse061 <wszafranski50505@gmail.com>
2026-01-22 12:10:38 +0100

    using usb delay?

--- C_serial_doer_stuffer/src/main.c
+++ C_serial_doer_stuffer/src/main.c
@@ -7,6 +7,25 @@
 const char *COM_PORT = "COM14";
 const int BAUD_RATE = 115200;
 
+#define I2C_Slave_Command_Reset_MCU 0x00		   // (universal)
+#define I2C_Slave_Command_Jump_To_Bootloader 0x01  // (universal)
+#define I2C_Slave_Command_Write_Flash 0x02		   // (universal)
+#define I2C_Slave_Command_Read_Flash 0x03		   // (universal)
+#define I2C_Slave_Command_Go_To_Flash_Address 0x04 // (universal)
+
+#define I2C_Slave_Command_GPIO_Set 0x10 // (dark_times)
+
+// USB Device Commands
+#define USB_Device_Command_I2C_Write 0x01
+#define USB_Device_Command_I2C_Send_Receive 0x02
+#define USB_Device_Command_Echo 0x03
+#define CMD_PING 0x04
+#define USB_Device_Command_Delay_Ms 0x05
+
+#define USB_Device_Command_PC_Short_Data_Return 0xFF
+
+#define TCA_ADDRESS 0x38
+
 int main()
 {
 	// 1. Setup Port
@@ -25,18 +44,25 @@ int main()
 	uint8_t echo_data[] = {0xDE, 0xAD, 0xBE, 0xEF};
 	USB_command_echo(echo_data, sizeof(echo_data));
 
-	// 4. I2C Write 1: 0x01 0x00 to 0x38
+	// 4. I2C Write 1: 0x01 0x00 to TCA_ADDRESS
 	uint8_t i2c_data1[] = {0x01, 0x00};
-	USB_command_i2c_write(0x38, i2c_data1, sizeof(i2c_data1));
+	USB_command_i2c_write(TCA_ADDRESS, i2c_data1, sizeof(i2c_data1));
 
 	// 5. Delay 1s
 	printf("... Delaying 1s ...\n");
 	// Using USB_wait_for_data with 1000ms timeout acts as a sleep if no data arrives
 	USB_wait_for_data(1000);
 
+	// TCA blink one command
+	uint8_t i2c_data_delay[] = {
+		5, USB_Device_Command_I2C_Write, TCA_ADDRESS, 0x01, 0x00 3, USB_Device_Command_Delay_Ms, 255,
+		5, USB_Device_Command_I2C_Write, TCA_ADDRESS, 0x01, 0xFF 3, USB_Device_Command_Delay_Ms, 255,
+		5, USB_Device_Command_I2C_Write, TCA_ADDRESS, 0x01, 0x00};
+	USB_write(i2c_data_delay, sizeof(i2c_data_delay));
+
 	// 6. I2C Write 2: 0x01 0xFF to 0x38
-	uint8_t i2c_data2[] = {0x01, 0xFF};
-	USB_command_i2c_write(0x38, i2c_data2, sizeof(i2c_data2));
+	uint8_t i2c_data2[] = {0x01, TCA_ADDRESS};
+	USB_command_i2c_write(TCA_ADDRESS, i2c_data2, sizeof(i2c_data2));
 
 	//  I2C Write TO ch32 2: 0x01 0xFF to 0x38
 	uint8_t i2c_data3[] = {0x10, 0xFF};
