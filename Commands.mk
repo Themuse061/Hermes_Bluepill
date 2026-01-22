87d5e83e454944d501bb818582daf7f2f16f2dc4
Themuse061 <wszafranski50505@gmail.com>
2026-01-22 11:15:55 +0100

    updated commands.mk

--- Commands.mk
+++ Commands.mk
@@ -1,49 +1,56 @@
-I2C commands
-0x00 - Reset MCU 			(universal)
-0x01 - Jump to bootloader	(universal)
-0x02 - Write Flash			(universal)
-0x03 - Read Flash			(universal)
-0x04 - Go to flash address	(universal)
+#ifndef COMMANDS_MK
+#define COMMANDS_MK
 
-0x10 - GPIO Set 			(dark_times)
+// I2C Slave Commands
+#define I2C_Slave_Command_Reset_MCU             0x00 // (universal)
+#define I2C_Slave_Command_Jump_To_Bootloader    0x01 // (universal)
+#define I2C_Slave_Command_Write_Flash           0x02 // (universal)
+#define I2C_Slave_Command_Read_Flash            0x03 // (universal)
+#define I2C_Slave_Command_Go_To_Flash_Address   0x04 // (universal)
 
-USB commands
-0x01 - I2C Write
-0x02 - I2C Send/Receive
-0x03 - Echo
-0x04 - Ping
-0x05 - delay_ms				(NOT IMPLEMENTED)
+#define I2C_Slave_Command_GPIO_Set              0x10 // (dark_times)
 
-0xFF - PC Short Data Return
+// USB Device Commands
+#define USB_Device_Command_I2C_Write            0x01
+#define USB_Device_Command_I2C_Send_Receive     0x02
+#define USB_Device_Command_Echo                 0x03
+#define USB_Device_Command_Ping                 0x04
+// #define USB_Device_Command_Delay_Ms          0x05 // (NOT IMPLEMENTED)
 
-Command description
-I2C
-0x10 - GPIO Set
-  Master sends: [Addr] [CMD_GPIO] [Bitmask]
-  Updates GPIO pins based on bitmask:
-  - Bit 0: PA2
-  - Bit 1: PD6
-  - Bit 2: PC4
+#define USB_Device_Command_PC_Short_Data_Return 0xFF
 
-USB
-0x01 - I2C Write
-  Writes data to I2C1 bus.
-  Format: [Packet Length] [0x01] [Target Address] [Data...]
+#endif // COMMANDS_MK
 
-0x02 - I2C Send/Receive
-  Writes data to I2C bus, then reads data back.
-  Format: [Packet Length] [0x02] [Target Address] [Write Length] [Read Length] [Write Data...]
-  Returns: [Packet Length] [0xFF] [0x02] [Address] [Read Data...]
+ Command description
+  I2C
+  0x10 - GPIO Set
+    Master sends: [Addr] [CMD_GPIO] [Bitmask]
+    Updates GPIO pins based on bitmask:
+    - Bit 0: PA2
+    - Bit 1: PD6
+    - Bit 2: PC4
 
-0x03 - Echo
-  Echoes the received packet back to the host.
-  Format: [Packet Length] [0x03] [Data...]
+  USB
+  0x01 - I2C Write
+    Writes data to I2C1 bus.
+    Format: [Packet Length] [0x01] [Target Address] [Data...]
 
-0x04 - Ping
-  Returns a fixed ping response.
-  Format: [Packet Length] [0x04] [Data...]
-  Response: 09 04 FF aa 00 11 00 aa FF
+  0x02 - I2C Send/Receive
+    Writes data to I2C bus, then reads data back.
+    Format: [Packet Length] [0x02] [Target Address] [Write Length] [Read Length] [Write Data...]
+    Returns: [Packet Length] [0xFF] [0x02] [Address] [Read Data...]
 
-0xFF - PC Short Data Return
-  Packet sent from MCU to PC containing requested data (e.g., from I2C Read).
-  Format: [Packet Length] [0xFF] [Source Command] [Address] [Data...]
+  0x03 - Echo
+    Echoes the received packet back to the host.
+    Format: [Packet Length] [0x03] [Data...]
+
+  0x04 - Ping
+    Returns a fixed ping response.
+    Format: [Packet Length] [0x04] [Data...]
+    Response: 09 04 FF aa 00 11 00 aa FF
+
+
+
+  0xFF - PC Short Data Return
+    Packet sent from MCU to PC containing requested data (e.g., from I2C Read).
+    Format: [Packet Length] [0xFF] [Source Command] [Address] [Data...]
